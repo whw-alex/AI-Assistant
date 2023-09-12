a7555a617efdf625382388d47185c84eb246f8db
leishaforlinminzhi <15611664060@qq.com>
2023-09-12 19:00:06 +0800

    用openai的api实现了生成图片

--- image_generate.py
+++ image_generate.py
@@ -1,11 +1,18 @@
 import os
+import openai
 
+# TODO： 更换api_key
+openai.api_key = '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'
+openai.api_base = "http://166.111.80.169:8080/v1"
 
 def image_generate(content: str):
-    """
-    TODO
-    """
-    pass
+
+    response = openai.Image.create(
+        prompt=content,
+        size="256x256"
+    )
+    img = response['data'][0]['url']
+    return img
 
 if __name__ == "__main__":
     image_generate('A cute baby sea otter')
