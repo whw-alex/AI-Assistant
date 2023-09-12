d4a8ceccf892db5afe3c3d060c5731b22a62d1ea
GuoKaku <guo-jw21@mails.tsinghua.edu.cn>
2023-09-12 22:48:29 +0800

    音转文&音频输出 ver 1.0 complete

--- tts.py
+++ tts.py
@@ -1,12 +1,23 @@
-
 import requests
 import os
+import json
 
-def text2audio(content: str):
+sound_path='./sounds/'
+
+def text2audio(content: str,filename):
     """
     TODO
     """
-    pass
+    
+    response=requests.post(url='http://166.111.80.169:8080/tts',data=json.dumps({
+      "input": content,
+      "model": "en-us-blizzard_lessac-medium.onnx",
+    }),headers={
+        'Content-Type': 'application/json'
+    })
+    with open(f'{sound_path+filename}','wb') as f:
+        f.write(response.content)
+
 
 if __name__ == "__main__":
-    text2audio("Sun Wukong (also known as the Great Sage of Qi Tian, Sun Xing Shi, and Dou Sheng Fu) is one of the main characters in the classical Chinese novel Journey to the West")
+    file=text2audio("Sun Wukong (also known as the Great Sage of Qi Tian, Sun Xing Shi, and Dou Sheng Fu) is one of the main characters in the classical Chinese novel Journey to the West")
